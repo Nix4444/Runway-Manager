9c08f2e9140bf91f618d19e5a35b034e99c156d6
rohis <rohitsaluja751@gmail.com>
2023-10-19 21:30:39 +0530

    Fleet Class
    contains functions for checking empty space in the fleet

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
 #include<iostream>
 using namespace std;
 
+<<<<<<< Updated upstream
 int main()
 {
     /* code */
     cout<<"Hello World";
+=======
+class Fleet {
+public:
+    int fleet[24];
+    int count;
+
+    Fleet() {
+        for (int i = 0; i < 24; i++) {
+            fleet[i] = -1; 
+        }
+        int count = 0; //keeps track of number of aircrafts in the fleet
+    }
+
+    bool empty_check() {
+        for (int i = 0; i < 24; i++) {
+            if (fleet[i] == -1) { //to find empty space
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+
+    int empty_index() {
+        if (empty_check()) {
+            for (int i = 0; i < 24; i++) {
+                if (fleet[i] == -1) { //to find the empty index
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+>>>>>>> Stashed changes
     return 0;
 }
